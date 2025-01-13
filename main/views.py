780ff4a3ed1eebfd6107503783e70c2cc9c09226
Jorge Arroba <149507738+jorfxd@users.noreply.github.com>
2025-01-13 19:53:37 +0000

    guia23

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import requests
+import json
 
 # Create your views here.
 from django.http import HttpResponse
@@ -6,4 +8,23 @@ from django.http import HttpResponse
 def index(request):
     #return HttpResponse("Hello, World!")
     #return render(request, 'main/base.html')
-    return render(request, 'main/index.html')
+    current_url = request.build_absolute_uri()
+    url = current_url + '/api/v1/landing'
+
+    response_http = requests.get(url)
+    response_dict = json.loads(response_http.content)
+
+    print("Endpoint ", url)
+    print("Response ", response_dict)
+
+    total_responses = len(response_dict.keys())
+
+    responses = response_dict.values()
+
+    data = {
+        'title': 'Landing - Dashboard',
+        'total_responses': total_responses,
+        'responses': responses
+     }
+
+    return render(request, 'main/index.html', data)
