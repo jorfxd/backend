2c305dac69065f200b3ff1ea20ba2fec147a6ad8
jorfxd <jaarroba@espol.edu.ec>
2025-01-29 17:54:54 -0500

    Grafico de pie
    ya funcionaa:D

--- static/js/charts-pie.js
+++ static/js/charts-pie.js
@@ -31,28 +31,50 @@ const pieConfig = {
 }
 
 // change this to the id of your chart element in HMTL
-const pieCtx = document.getElementById('pie')
-window.myPie = new Chart(pieCtx, pieConfig)
+const pieCtx = document.getElementById('pie');
+window.myPie = new Chart(pieCtx, pieConfig);
 
 countCommentsByHour = (data) => {
-
   // Inicializar contadores por rango de horas
   const labels = ["0 a.m. - 8 a.m.", "8 a.m. - 16 p.m.", "16 p.m. - 0 a.m."];
   const counts = [0, 0, 0];
 
   Object.values(data).forEach(record => {
-
       const savedTime = record.saved;
       if (!savedTime) {
           return;
       }
 
-      // Convertir a formato de hora AM/PM
-      const formattedTime = savedTime.replace('a. m.', 'AM').replace('p. m.', 'PM');
-         
+      console.log(`Hora: ${savedTime}`);
+
+      // Convertir la hora a un formato AM/PM estándar
+      let formattedTime = savedTime.replace('a. m.', 'AM').replace('p. m.', 'PM');
+
+      console.log(`Hora formateada: ${formattedTime}`);
+
+      // Reemplazar la coma por espacio para poder usarla en la fecha
+      formattedTime = formattedTime.replace(',', '');
+
+      // Crear la fecha en formato 'YYYY-MM-DD HH:MM:SS AM/PM' que JavaScript puede interpretar
+      const formattedDate = formattedTime.replace(/(\d{2})\/(\d{2})\/(\d{4}) (\d{1,2}):(\d{2}):(\d{2}) (AM|PM)/, 
+          '$3-$2-$1 $4:$5:$6 $7');
+
+      console.log(`Hora formateada a formato legible: ${formattedDate}`);
+
       // Crear objeto Date con la cadena de tiempo
-      const dt = new Date(Date.parse(formattedTime.replace(/(\d{2}\/\d{2}\/\d{4}), (\d{2}):(\d{2}):(\d{2}) (AM|PM)/, '$1 $2:$3:$4 $5')));
+      const dt = new Date(formattedDate);
+
+      console.log(`Hora procesada a objeto Date: ${dt}`);
+
+      // Verificar si la hora es válida
+      if (isNaN(dt)) {
+          console.log('Error al procesar la hora');
+          return;
+      }
+
+      // Obtener la hora en formato de 24 horas
       const hour = dt.getHours();
+      console.log(`Hora a formato hour: ${hour}`);
 
       // Clasificar en el rango correspondiente
       if (hour >= 0 && hour < 8) {
@@ -64,8 +86,12 @@ countCommentsByHour = (data) => {
       }
   });
 
+  console.log(`contador de array: ${counts}`);
   return { labels, counts };
-}
+};
+
+
+
 
 update = () => {
   fetch('/api/v1/landing')
